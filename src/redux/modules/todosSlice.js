6ce97015f2071f0b13d747fe4708b49ce0ccc698
jia222 <112179965+jia222@users.noreply.github.com>
2023-07-13 02:18:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(addTodo(payload));
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(deleteTodo(payload));
+  }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
+    },
+    //여기도 추가해줌
+    deleteTodo: (state, action) => {
+      state.list.map((item) => console.log(item.id));
+      console.log(action.payload);
+      state.list = state.list.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
